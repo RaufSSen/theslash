dbea650c4649a0fbfa9b2da7e0041781e82cce25
RaufSSen <33981808+RaufSSen@users.noreply.github.com>
2019-04-29 20:26:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Aktifim vede Sunucunda Onlineyim!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '!yardım') {
+    	message.reply('Sana belediye baksın mal.');
   	}
 });
 
